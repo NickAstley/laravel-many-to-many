4ddf90fc989a284f30dc587eaf5d956045ff670a
NickAstley <andreanicastro1994@gmail.com>
2022-08-24 23:59:36 +0200

    Created migrations for tags table and pivot table

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Tag;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ["tecnologia", "cucina", "attualità", "storia", "politica"];
+        foreach($tags as $tag) {
+            Tag::create([
+                "name" => $tag,
+                "slug" => Str::slug($tag)
+            ]);
+        }
+    }
+}
